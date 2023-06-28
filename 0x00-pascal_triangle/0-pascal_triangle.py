167ac1183b909435402236b26dbfa5575dc4c8d9
Godwin Kingsley Woode <68077816+gkwoode@users.noreply.github.com>
2023-06-28 23:37:00 +0000

    Pascal Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Pascal Triangle Script"""
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        prev_row = triangle[i - 1]
+        curr_row = [1]
+
+        for j in range(1, i):
+            curr_row.append(prev_row[j - 1] + prev_row[j])
+
+        curr_row.append(1)
+        triangle.append(curr_row)
+
+    return triangle
