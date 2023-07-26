9b8b9bc90e1c6b4e9ca54f63f4d51eb35b00a865
gkwoode <winsleywoode@gmail.com>
2023-07-26 23:05:49 +0000

    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+
+def validUTF8(data):
+    # Helper function to check if a number has the '10' prefix
+    def is_10_prefix(num):
+        return (num & 0b11000000) == 0b10000000
+
+    # Number of bytes remaining for the current character
+    remaining_bytes = 0
+
+    for num in data:
+        # If no remaining bytes, check the starting byte of a new character
+        if remaining_bytes == 0:
+            if (num & 0b10000000) == 0:
+                continue  # 1-byte character, move to the next number
+            elif (num & 0b11100000) == 0b11000000:
+                remaining_bytes = 1
+            elif (num & 0b11110000) == 0b11100000:
+                remaining_bytes = 2
+            elif (num & 0b11111000) == 0b11110000:
+                remaining_bytes = 3
+            else:
+                return False  # Invalid starting byte for UTF-8 character
+        else:
+            # If it's not a continuation byte, it's an invalid character
+            if not is_10_prefix(num):
+                return False
+            remaining_bytes -= 1
+
+    # All bytes have been checked and no incomplete characters are left
+    return remaining_bytes == 0
+
+# Test case
+if __name__ == "__main__":
+    data_set = [197, 130, 1]
+    print(validUTF8(data_set))  # Output should be True
